32fe08d2cb4892c14de4aa14db003dfbf9c2f2e4
Woodelin Florveus <woodelinflorveus@gmail.com>
2020-10-27 12:25:09 +0000

    Done.

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,23 @@
+def using_concat(str1,str2)
+  str1.concat(str2)
+end
+
+def using_insert(array,element)
+  array.insert(4,element)
+end
+
+def using_uniq(array)
+  array.uniq
+end
+
+def using_flatten(array)
+  array.flatten
+end
+
+def using_delete(array,string)
+  array.delete(string)
+end
+
+def using_delete_at(array, integer)
+  array.delete_at(integer)
+end
